0a9451f7c8e8fbf59f0f0b862f1e0ce85859c56f
swathi-22 <mkswathisuresh@gmail.com>
2022-06-16 17:24:44 +0530

    Page setting

--- web/urls.py
+++ web/urls.py
@@ -7,4 +7,8 @@ app_name='web'
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('about/', views.about,name='about'),
+    path('product/', views.product,name='product'),
+    path('gallery/', views.gallery,name='gallery'),
+    path('contact/', views.contact,name='contact'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+
+    }
+    return render(request,'web/index.html',context)
+
+
+def about(request):
+    context = {
+
+    }
+    return render(request,'web/about.html',context)
+
+
+def product(request):
+    context = {
+
+    }
+    return render(request,'web/product.html',context)
+
+
+def gallery(request):
+    context = {
+
+    }
+    return render(request,'web/gallery.html',context)
+
+
+def contact(request):
+    context = {
+
+    }
+    return render(request,'web/contact.html',context)
